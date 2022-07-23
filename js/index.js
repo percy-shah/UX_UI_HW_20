ec57718f3f75f74008f61821d0e8731bfd732169
percy-shah <persephone.shah1@gmail.com>
2022-07-23 14:58:44 -0500

    added links to TRASAR case study

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function fade() {
+    $(".preloader").fadeOut("slow");
+     }
+   setTimeout(fade,3000);
+
+   function mouseOver() {
+    document.getElementById("contactParent").style.backgroundColor="#A7B4DD";
+    object.style.color = "black";
+    
+  }
+  
+  function mouseOut() {
+    document.getElementById("contactParent").style.backgroundColor="#4E5C80";
+    object.style.color = "white";
+  }
+
+ 
